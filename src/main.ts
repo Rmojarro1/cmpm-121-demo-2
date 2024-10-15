1fab8a86107cc0504ebfa0875eec4a43e21f37e5
Rmojarro1 <48000819+Rmojarro1@users.noreply.github.com>
2024-10-14 17:25:42 -0700

    Step 1 Actually Complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,26 @@ const APP_NAME = "Raul's D2";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//app.innerHTML = APP_NAME;
+
+const gameName = "Raul's D2";    
+
+const header = document.createElement("h1"); 
+header.innerHTML = gameName; 
+app.append(header); 
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement; 
+canvas.width = 256; 
+canvas.height = 256; 
+const ctx = canvas.getContext('2d')!; 
+
+ 
+if (ctx) {
+    ctx.shadowColor = 'grey'; 
+    ctx.shadowBlur= 50; 
+    ctx.shadowOffsetX = -5; 
+    ctx.shadowOffsetY = 5;  
+
+    ctx.fillStyle = 'lightgrey';
+    ctx.fillRect(0, 0, 250, 250); 
+}
